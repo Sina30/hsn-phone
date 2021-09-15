34c837f37f50b3119110bab53fd382e1195b4666
Coconut Watermelon <90282318+coconutwatermelon@users.noreply.github.com>
2021-09-15 15:05:23 -0700

    Crypto Removal / Collate change

--- Database.sql
+++ Database.sql
@@ -5,18 +5,10 @@ CREATE TABLE `hsn_phone_ambulancepatients` (
 	`date` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
 	`id` INT(11) NOT NULL
 )
-COLLATE='utf8_turkish_ci'
+COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 ;
 
-CREATE TABLE `hsn_phone_cryptotransactions` (
-	`owner` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
-	`app` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
-	`amount` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_turkish_ci'
-)
-COLLATE='utf8_turkish_ci'
-ENGINE=InnoDB
-;
 
 CREATE TABLE `hsn_phone_mails` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -26,7 +18,7 @@ CREATE TABLE `hsn_phone_mails` (
 	`app` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='utf8_turkish_ci'
+COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=26
 ;
@@ -37,7 +29,7 @@ CREATE TABLE `hsn_phone_messages` (
 	`data` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
 	`date` TIMESTAMP NULL DEFAULT current_timestamp()
 )
-COLLATE='utf8_turkish_ci'
+COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 ;
 
@@ -47,7 +39,7 @@ CREATE TABLE `hsn_phone_notes` (
 	`noteData` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='utf8_turkish_ci'
+COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=94
 ;
@@ -64,7 +56,7 @@ CREATE TABLE `hsn_phone_tweets` (
 	`date` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='utf8_turkish_ci'
+COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=181
 ;
@@ -75,7 +67,7 @@ CREATE TABLE `hsn_phone_twitter_accounts` (
 	`owner` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_turkish_ci',
 	`photo` LONGTEXT NULL DEFAULT '' COLLATE 'utf8_turkish_ci'
 )
-COLLATE='utf8_turkish_ci'
+COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 ;
 
@@ -83,8 +75,5 @@ ALTER TABLE `users` ADD COLUMN `phone_number` VARCHAR(10) NULL;
 ALTER TABLE `users` ADD COLUMN `iban` VARCHAR(50) NULL;
 ALTER TABLE `users` ADD COLUMN `phonedata` LONGTEXT;
 ALTER TABLE `users` ADD COLUMN `contacts` LONGTEXT;
-ALTER TABLE `users` ADD COLUMN `cryptocurrency` LONGTEXT NULL DEFAULT '{"devcoin":0.0,"ggcoin":0.0,"ethereum":0.0,"bitcoin":0.0}';
-ALTER TABLE `users` ADD COLUMN `cryptocurrencytransfers` LONGTEXT;
-ALTER TABLE `users` ADD COLUMN `cryptoid` LONGTEXT;
 ALTER TABLE `users` ADD COLUMN `bolos` LONGTEXT;
 ALTER TABLE `users` ADD COLUMN `downloadedapps` LONGTEXT;
